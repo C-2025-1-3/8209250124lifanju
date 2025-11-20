afc10086eb434e3bd2e7f40d889e8201f205ca84
twobythe2 <2445877730@qq.com>
2025-11-20 20:05:12 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -1,13 +1,18 @@
-#include <iostream>
-#include <iomanip>
+#include<iostream>
 using namespace std;
-
-int main() {
-    double fahrenheit, celsius;
-    cout << "请输入华氏温度：";
-    cin >> fahrenheit;
-    // 华氏转摄氏公式：C = (F - 32) * 5 / 9
-    celsius = (fahrenheit - 32) * 5.0 / 9;
-    cout << "转换后的摄氏温度为：" << fixed << setprecision(2) << celsius << "℃" << endl;
-    return 0;
+int main()
+{
+	double price = 0.8;
+	int day = 1;
+	int apples = 2;
+	int totalApples = 0;
+	double totalCost = 0;
+	while (apples <= 100)
+		totalApples += apples;
+	totalCost += apples * price;
+	day++;
+	apples * 2;
+	double averageCost = totalCost / (day - 1);
+	cout << "每天平均花费:" << averageCost <<"元" << endl;
+	return 0;
 }
